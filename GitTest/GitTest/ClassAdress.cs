c4f88004d844802cc7a35af7060b3a72ff4411c3
Tatiana <eta37979@gmail.com>
2020-09-08 20:32:21 +0300

    Added class Address

---
+++ GitTest/GitTest/ClassAdress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    class ClassAdress
+    {
+        public string Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+
+    }
+
+}
