218d0522ceef71e67f53fe2fe361f8dc7e55ffd6
Samtrek <Samtrek@HOME-PC>
2020-09-08 20:32:07 +0300

    Добавил Класс Hostel с ID и Name

---
+++ GitTest/GitTest/Hostel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace GitTest
+{
+    class Hostel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
