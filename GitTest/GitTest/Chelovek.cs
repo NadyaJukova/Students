373ee9dbb266eddbb345c74e37e5a7a96590dca9
Денис <d.zadniprovsky4572@gmail.com>
2020-09-08 20:33:00 +0300

    Created new class Chelovek

---
+++ GitTest/GitTest/Chelovek.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    public class Chelovek
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
