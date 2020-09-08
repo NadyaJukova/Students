1ef9b5c2869dffc97b148efe1d38531e9503c5df
Natalya <dorsonkhome@gmail.com>
2020-09-08 21:35:52 +0300

    Adding Interface IDrawable to class Subjects

--- GitTest/GitTest/Subject.cs
+++ GitTest/GitTest/Subject.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace GitTest
 {
-    class Subject
+    class Subject:IDrawable
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        
+        public void Draw()
+        {
+            Console.WriteLine($"Class:User. Value:ID:{this.Id},Name:{this.Name}");
+        }
     }
 }
