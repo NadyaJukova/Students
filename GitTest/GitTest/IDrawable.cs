bde2e4b59bb601ba485b14d536874110efa803ec
AlexL <alex.ledok@gmail.com>
2020-09-08 20:29:23 +0300

    Added Class user and base infrastructure

---
+++ GitTest/GitTest/IDrawable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    interface IDrawable
+    {
+        void Draw();
+    }
+}
---
+++ GitTest/GitTest/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    class User : IDrawable
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public void Draw()
+        {
+            Console.WriteLine($"Class:User. Value:ID:{this.Id},Name:{this.Name}");
+        }
+    }
+}
